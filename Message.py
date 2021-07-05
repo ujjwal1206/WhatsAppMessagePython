bc2db0c7082e484c434dd674cce5fc79102ca3cd
ujjwal1206 <ujjwal1985.cse18@chitkara.edu.in>
2021-07-05 15:26:10 +0530

    whatsapp message using python

---
+++ Message.py
@@ -0,0 +1,5 @@
+import pywhatkit as kit
+#pip install pywahtkit
+kit.sendwhatmsg("+91XXXXXXXXXX","Message",15,12)
+#                    ||              ||     ||
+# Mobile Number with country code  Message Time Hours,Minutes
